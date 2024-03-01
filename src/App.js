0f43b8f761bf870e5a35dfde44b8705b6ee54db2
Ian Marx <iongonga@kabarak.ac.ke>
2024-03-02 02:26:37 +0300

    Pie chart render to main

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Contacts from "./scenes/contacts";
 import Form from "./scenes/form";
 import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
+import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
 // import Geography from "./scenes/qeography";
 import Calendar from "./scenes/calendar";
@@ -39,8 +39,8 @@ function App() {
               <Route path="/calendar" element={<Calendar/>} />
               <Route path="/faq" element={<FAQ/>} />
               <Route path="/bar" element={<Bar/>} />
-              {/*
               <Route path="/pie" element={<Pie/>} />
+              {/*
               <Route path="/line" element={<Line/>} />
               <Route path="/geography" element={<Geography/>} />
                */}
