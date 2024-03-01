3990501bf2af5d84991a3f0d3a6cb8cc12f26dee
Ian Marx <iongonga@kabarak.ac.ke>
2024-03-02 02:10:59 +0300

    Bar chart render to main

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 import Form from "./scenes/form";
-// import Bar from "./scenes/bar";
+import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
@@ -38,8 +38,8 @@ function App() {
               <Route path="/form" element={<Form/>} />
               <Route path="/calendar" element={<Calendar/>} />
               <Route path="/faq" element={<FAQ/>} />
-              {/*
               <Route path="/bar" element={<Bar/>} />
+              {/*
               <Route path="/pie" element={<Pie/>} />
               <Route path="/line" element={<Line/>} />
               <Route path="/geography" element={<Geography/>} />
