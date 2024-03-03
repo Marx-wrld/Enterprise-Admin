2442cf30348bc435da0a3f5408233b74d5cd443d
Ian Marx <iongonga@kabarak.ac.ke>
2024-03-03 22:11:25 +0300

    Progress Circle import fix and content alignment fix

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -7,11 +7,11 @@ import { Email } from "@mui/icons-material";
 import { PointOfSale } from "@mui/icons-material";
 import { PersonAdd } from "@mui/icons-material";
 import { Traffic } from "@mui/icons-material";
-import LineChart from "../../components/GeographyChart"
+import LineChart from "../../components/LineChart"
 import BarChart from "../../components/BarChart";
 import GeographyChart from "../../components/GeographyChart";
 import StatBox from "../../components/StatBox";
-import ProgressCircle from "../../components/ProgressCircles";
+import ProgressCircle from "../../components/ProgressCircle";
 
 const Dashboard= () => {
 
@@ -22,21 +22,21 @@ const Dashboard= () => {
     <Box m="20px">
         <Box display="flex" justifyContent="space-between" alignItems="center">
         <Header title="DASHBOARD" subtitle="Welcome to your Dashboard" />
-        </Box>
+        
         <Box>
             <Button
                 sx={{
-                    backgroundColor: colors.blueAccent[700], color: colors.grey[100], fontSize: "14px", fontWeight: "bold", borderRadius: "20px", padding: "10px 20px", textTransform: "none",
+                    backgroundColor: colors.blueAccent[700], color: colors.grey[100], fontSize: "14px", fontWeight: "bold", borderRadius: "8px", padding: "10px 20px", textTransform: "none",
                 }}
             >
                 <DownloadOutlined 
                     sx={{
                         mr: "10px",
                     }}
-                >
+                />
                     Download Reports
-                </DownloadOutlined>
             </Button>
+        </Box>
         </Box>
 
         {/* Grid & Charts */}
@@ -111,6 +111,114 @@ const Dashboard= () => {
             
             {/* Row 2 */}
 
+            <Box 
+                gridColumn="span 8"
+                gridRow="span 2"
+                backgroundColor={colors.primary[400]}
+            >
+                <Box
+                    mt="25px"
+                    p="0 30px"
+                    display="flex"
+                    justifyContent="space-between"
+                    alignItems="center"
+                >
+                    <Box>
+                        <Typography 
+                            variant="h5"
+                            fontWeight="600"
+                            color={colors.grey[100]}
+                        >
+                            Revenue Generated
+                        </Typography>
+
+                        <Typography 
+                            variant="h3"
+                            fontWeight="500"
+                            color={colors.greenAccent[500]}
+                        >
+                            $5,934,232
+                        </Typography>
+                    </Box>
+                    <Box>
+                        <IconButton>
+                            <DownloadOutlined 
+                            sx={{
+                                fontSize: "26px",
+                                color: colors.greenAccent[500],
+                            }}
+                            />
+                        </IconButton>
+                    </Box>
+                </Box>
+
+                <Box height="210px" mt="-20px 0 0 0">
+                    <LineChart isDashboard={true} />
+                </Box>
+            </Box>
+
+                {/* Transactions */}
+
+                <Box
+                    gridColumn="span 4"
+                    gridRow="span 2"
+                    backgroundColor={colors.primary[400]}
+                    overflow="auto"
+                >
+                    <Box 
+                        display="flex" justifyContent="space-between" 
+                        alignItems="center"
+                        borderBottom={`4px solid ${colors.primary[500]}`} colors={colors.grey[100]} p="15px"
+                    >
+                        <Typography 
+                            color={colors.grey[100]}
+                            variant="h5"
+                            fontWeight="600"
+                        >
+                            Recent Transactions
+                        </Typography>
+                    </Box>
+                    {mockTransactions.map((transaction, index) => (
+                        <Box
+                            key={`${transaction.txId}-${index}`}
+                            display="flex"
+                            justifyContent="space-between"
+                            alignItems="center"
+                            borderBottom={`4px solid ${colors.primary[500]}`}
+                            colors={colors.grey[100]} 
+                            p="15px"
+                        >
+                            <Box>
+                                <Typography 
+                                    color={colors.greenAccent[500]}
+                                    variant="h5"
+                                    fontWeight="600"
+                                >
+                                    {transaction.txId}
+                                </Typography>
+                                <Typography
+                                    color={colors.grey[100]}
+                                >
+                                    {transaction.user}
+                                </Typography>
+                            </Box>
+
+                            <Box
+                                color={colors.grey[100]}
+                            >
+                                {transaction.date}
+                            </Box>
+                            <Box
+                                backgroundColor={colors.greenAccent[500]}
+                                p="5px 10px"
+                                borderRadius="4px"
+                                gap="5px"
+                            >
+                                ${transaction.cost}
+                            </Box>
+                        </Box>
+                    ))}
+                </Box>
         </Box>
     </Box>
     );
