3a6cb566862f61c23359b0f2016d262332d30d4e
Ian Marx <iongonga@kabarak.ac.ke>
2024-03-03 22:43:24 +0300

    Graticule fix

--- src/components/GeographyChart.jsx
+++ src/components/GeographyChart.jsx
@@ -48,7 +48,6 @@ const GeographyChart = ( {isDashboard = false} ) => {
         projectionScale={isDashboard ? 40 : 150}
         projectionTranslation={isDashboard ? [0.49, 0.6 ] : [ 0.5, 0.5 ]}
         projectionRotation={[ 0, 0, 0 ]}
-        enableGraticule={true}
         borderWidth={1.5}
         borderColor="#152538"
         legends={
