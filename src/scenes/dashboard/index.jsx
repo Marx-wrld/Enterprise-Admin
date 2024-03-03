fa89fc377490962fdf5c579ad4f653f1b699d1de
Ian Marx <iongonga@kabarak.ac.ke>
2024-03-03 22:44:05 +0300

    Sales quantity and geography based sections

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -134,7 +134,7 @@ const Dashboard= () => {
 
                         <Typography 
                             variant="h3"
-                            fontWeight="500"
+                            fontWeight="bold"
                             color={colors.greenAccent[500]}
                         >
                             $5,934,232
@@ -152,7 +152,7 @@ const Dashboard= () => {
                     </Box>
                 </Box>
 
-                <Box height="210px" mt="-20px">
+                <Box height="210px" mt="-20px 0 0 0">
                     <LineChart isDashboard={true} />
                 </Box>
             </Box>
@@ -218,6 +218,86 @@ const Dashboard= () => {
                             </Box>
                         </Box>
                     ))}
+                </Box>
+
+                {/* Row 3 */}
+
+                <Box 
+                    gridColumn="span 4"
+                    gridRow="span 2"
+                    backgroundColor={colors.primary[400]}
+                    p="30px"
+                >
+                    <Typography
+                        variant="h5"
+                        fontWeight="600"
+                    >
+                        Campaign
+                    </Typography>
+                    <Box
+                        display="flex"
+                        flexDirection="column"
+                        alignItems="center"
+                        mt="25px"
+                    >
+                        <ProgressCircle size="125" />
+                        <Typography
+                            variant="h5"
+                            color={colors.greenAccent[500]}
+                            sx={{
+                                mt: "15px"
+                            }}
+                        >
+                            $48,352 revenue generated
+                        </Typography>
+                        <Typography>
+                            Includes extra miscellaneous expenditures and costs
+                        </Typography>
+                    </Box>
+                </Box>
+
+                <Box 
+                    gridColumn="span 4"
+                    gridRow="span 2"
+                    backgroundColor={colors.primary[400]}
+                >
+                    <Typography
+                        variant="h5"
+                        fontWeight="600"
+                        sx={{
+                            p: "30px 30px 0 30px"
+                        }}
+                    >
+                        Sales Quantity
+                    </Typography>
+                    <Box
+                        height="250px"
+                        mt="-20px"
+                    >
+                        <BarChart isDashboard={true} />
+                    </Box>
+                </Box>
+
+                <Box 
+                    gridColumn="span 4"
+                    gridRow="span 2"
+                    backgroundColor={colors.primary[400]}
+                    p="30px"
+                >
+                    <Typography
+                        variant="h5"
+                        fontWeight="600"
+                        sx={{
+                            mb: "15px"
+                        }}
+                    >
+                        Geography Based Traffic
+                    </Typography>
+                    <Box
+                        height="200px"
+                    >
+                        <GeographyChart isDashboard={true} />
+                    </Box>
                 </Box>
         </Box>
     </Box>
